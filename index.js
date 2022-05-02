5985af251542686853d82c4575d6dade36b4312c
Dmitry S <dmitry.s@hsdn.org>
2022-05-02 05:39:44 +0400

    p92

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 module.exports = function SwearWords(mod) {
-  const unfilter = (e) => {
-    e.message = e.message.replace(/<FONT>(.*?)<\/FONT>/g, "<FONT></FONT>$1");
-    return true;
-  };
+	const unfilter = (e) => {
+		e.message = e.message.replace(/<FONT>(.*?)<\/FONT>/g, "<FONT></FONT>$1");
+		return true;
+	};
 
-  for (const data of [
-    ["S_CHAT", 4],
-    ["S_WHISPER", 4],
-    ["S_PRIVATE_CHAT", 1],
-    ["C_CHAT", 1],
-    ["C_WHISPER", 2],
-  ]) {
-    mod.hook(...data, { order: 100 }, unfilter);
-  }
+	for (const data of [
+		["S_CHAT", 3],
+		["S_WHISPER", 3],
+		["S_PRIVATE_CHAT", 1],
+		["C_CHAT", 1],
+		["C_WHISPER", 1],
+	]) {
+		mod.hook(...data, { order: 100 }, unfilter);
+	}
 };
